a3f252b693ccd1d8afc95b8f3dbebd1762686149
Jules Topart <jules.topart@gmail.com>
2018-09-12 12:04:40 +0200

    Botly update

--- src/Scott.cpp
+++ src/Scott.cpp
@@ -10,28 +10,17 @@ Scott::Scott(int version){
 	Steppers = new ScottSteppers();
 }
 
-void Scott::init(){
-  tpsTop = millis();
-  crayon.attach(_pinServo);
-  crayon.write(_haut);
-  
-  pinMode(_pinSwitchDroite, INPUT);
-  pinMode(_pinSwitchGauche, INPUT);
-  
-  pinMode(_pinLigneDroite, INPUT);
-  pinMode(_pinLigneGauche, INPUT);
-  
-  pinMode(_pinLumiereDroite, INPUT);
-  pinMode(_pinLumiereGauche, INPUT);
- 
-  pinMode(_pinDistDroite, INPUT);
-  pinMode(_pinDistGauche, INPUT);
-  
-  pinMode(_pinIrEmetteur, OUTPUT);
-  
-  Steppers->setMaxSpeed(900.0);
-  Steppers->setSpeed(300.0);
-  Steppers->enable();
+void Scott::init()
+{	
+	analogReference(INTERNAL); //reference analogique 2.56V
+	
+	tpsTop = millis();
+	crayon.attach(_pinServo);
+	crayon.write(_haut);
+   
+	Steppers->setMaxSpeed(900.0);
+	Steppers->setSpeed(300.0);
+	Steppers->enable();
 }
 
 void Scott::run(){
@@ -101,6 +90,7 @@ void Scott::gauche(long pas){
 	Steppers->moveTo(-pas, -pas);
 	Steppers->runSpeedToPosition();//Blockling...
 	Steppers->setPositions();
+	
 }
 
 void Scott::droite(long pas){
@@ -123,7 +113,10 @@ void Scott::stop(long temps){
 	}
 }
 
-
+//Battery Power save !!!!
+void Scott::stop(){
+	Steppers->disable();
+}
 
 
 void Scott::tournerGauche(long angleDegree){
@@ -185,54 +178,8 @@ void Scott::arc( float rayon,float angle){
 	Steppers->moveTo(pasD, pasG);
 }
 
-unsigned char Scott::lectureContact(){
-	return (!digitalRead(_pinSwitchDroite) + 2*(!digitalRead(_pinSwitchGauche)));
-	//  Gauche  |  Droit  ||  Resultat
-	//----------|---------||----------
-	//    0     +    0    ||     0
-	//    0     +    1    ||     1
-	// 2 (2*1)  +    0    ||     2
-	// 2 (2*1)  +    1    ||     3
-}
 
-unsigned int Scott::lectureLumiere(){
-	delayMicroseconds(180);
 
-	unsigned int _LumiereDroite = analogRead(_pinLumiereDroite);
-	unsigned int _LumiereGauche = analogRead(_pinLumiereGauche);
-
-	return (_LumiereDroite*100)/(_LumiereGauche + _LumiereDroite);
-
-}
-
-unsigned int Scott::lectureDistance(){
-	digitalWrite(_pinIrEmetteur,HIGH);
-	delayMicroseconds(180);
-
-	_distDroite = analogRead(_pinDistDroite);
-	_distGauche = analogRead(_pinDistGauche);
-
-	digitalWrite(_pinIrEmetteur,LOW);
-	delayMicroseconds(180);
-	_distDroite -= analogRead(_pinDistDroite);
-	_distGauche -= analogRead(_pinDistGauche);
-
-	return (_distDroite*100)/(_distGauche + _distDroite);
-}
-
-unsigned int Scott::lectureLigne(){
-	digitalWrite(_pinIrEmetteur,HIGH);
-	delayMicroseconds(180);
-	unsigned int _irDroit = analogRead(_pinLigneDroite);
-	unsigned int _irGauche = analogRead(_pinLigneGauche);
-
-	digitalWrite(_pinIrEmetteur,LOW);
-	delayMicroseconds(180);
-	_irDroit -= analogRead(_pinLigneDroite);
-	_irGauche -= analogRead(_pinLigneGauche);
-
-	return (_irDroit*100)/(_irGauche + _irDroit);
-}
 
 void Scott::leverCrayon(){
 	crayon.write(_haut);
@@ -247,3 +194,126 @@ void Scott::bougerCrayon(int angle){
 }
 
 
+
+
+
+
+
+void Scott::isIRDataReceived() {
+	if (irrecv.decode(&results)) {
+    Serial.println(results.value, HEX);
+    irrecv.resume(); // Receive the next value
+    }
+}
+
+void Scott::initIRcom() {
+	irrecv.enableIRIn(); // Start the receiver
+}
+
+void Scott::sonyCode(byte data) {
+	irsend.sendSony(data, 8);
+}
+
+bool Scott::proximite() {
+	for(int i = 0; i <= 384; i++) { //envoie une trame
+		digitalWrite(_pinIrEmetteur, HIGH);
+		delayMicroseconds(13);
+		digitalWrite(_pinIrEmetteur, LOW);
+		delayMicroseconds(13);
+	}
+	
+	if(digitalRead(_pinTsop)==LOW) { //on regarde si le tsop détecte la trame
+      return 1;
+    }
+	
+    else {
+      return 0;
+    } 
+}
+
+/*Cette fonction mesure la valeur analogique
+La référence interne est de 2.56V peu importe la tension
+d'alimentation du controleur
+Mesure sur 1024 valeurs (10bits)
+Méthode de recalcul: (2.56/1024)*mesureAnalogique
+Ne pas oublier le rapport de transformation du pont diviseur
+en hardware
+*/
+int Scott::mesureBatterie()
+{
+	
+	int mesureAnalogique=analogRead(_pinMesureBatterie);
+	return mesureAnalogique;
+}
+
+void Scott::sleepNow()
+{
+/* In the Atmega32u4 datasheet on page 62
+ * there is a list of sleep modes which explains which clocks and
+ * wake up sources are available for each sleep mode.
+ *
+ * In the avr/sleep.h file, the call names of these sleep modus are to be found:
+ *
+ * The 5 different modes are:
+ * SLEEP_MODE_IDLE -the least power savings, wake up on usart
+ * SLEEP_MODE_ADC
+ * SLEEP_MODE_PWR_SAVE
+ * SLEEP_MODE_STANDBY
+ * SLEEP_MODE_PWR_DOWN -the most power savings
+ */
+
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
+	sleep_enable(); // enables the sleep in mcucr register,safety bit
+	power_all_disable(); //low power consumption
+	sleep_mode(); // here the device is actually put to sleep!!
+	// THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
+	sleep_disable(); //disable sleep, safety bit
+	power_all_enable(); //restore power on peripherals
+}
+
+void Scott::sleepWakeup()
+{
+/* In the Atmega32u4 datasheet on page 62
+ * there is a list of sleep modes which explains which clocks and
+ * wake up sources are available for each sleep mode.
+ *
+ * In the avr/sleep.h file, the call names of these sleep modus are to be found:
+ *
+ * The 5 different modes are:
+ * SLEEP_MODE_IDLE -the least power savings, wake up on usart
+ * SLEEP_MODE_ADC
+ * SLEEP_MODE_PWR_SAVE
+ * SLEEP_MODE_STANDBY
+ * SLEEP_MODE_PWR_DOWN -the most power savings
+ */
+
+	attachInterrupt(0, pin2_isr,LOW );//wake up on interrupt pin 2 or 3
+	set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode
+	cli(); //clear interrupts
+	sleep_enable(); // enables sleep bit in mcucr register, safety pin
+	sei();//to allow wake up by interrupt
+	//power_adc_disable();
+	//power_usart0_disable();
+	//power_spi_disable();
+	//power_twi_disable();
+	//power_timer0_disable();
+	//power_timer1_disable();
+	//power_timer2_disable();
+	//power_timer3_disable();
+	//power_usart1_disable();
+	//power_usb_disable();
+	
+	power_all_disable(); //low power consumption
+	sleep_mode(); // here the device is actually put to sleep!!
+	// THE PROGRAM CONTINUES FROM HERE AFTER WAKING UP
+	sleep_disable(); //disable sleep,safety bit
+	power_all_enable(); //restore power on peripherals
+}
+
+//class extern
+//external interrupt routine to wake up controller
+void pin2_isr()
+{
+  sleep_disable();
+  detachInterrupt(0);
+}
